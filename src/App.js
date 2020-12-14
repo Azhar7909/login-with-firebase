aafcdb8e15ff39a6d204a9e8186eec3923a2aee3
Muhammad Azhar Hussain <44233088+Azhar7909@users.noreply.github.com>
2020-12-14 19:14:27 +0500

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import LoginPage from './components/LoginPage';
+import SignupPage from './components/SignupPage';
+
 
 function App() {
+  const [componentRender, setComponentRender] = useState(true)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="width-height margin-auto">
+      <div className="d-flex">
+        <h4 onClick={()=>setComponentRender(true)} className={`width-50 p-2 rounded-start text-center pointer ${componentRender? "bg-dark text-white" : ""}`}>Login</h4>
+        <h4 onClick={()=>setComponentRender(false)} className={`width-50 p-2 rounded-end text-center pointer ${componentRender? "" : "bg-dark text-white"}`}>Signin</h4>
+      </div>
+      {componentRender? <LoginPage /> : <SignupPage />}
     </div>
   );
 }
